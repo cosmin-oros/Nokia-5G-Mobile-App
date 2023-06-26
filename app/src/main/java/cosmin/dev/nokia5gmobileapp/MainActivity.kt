ae23fd8bbb34338193cb6e0d91d950951b4c93eb
Cosmin <oroscosmin09@gmail.com>
2023-06-26 16:41:51 +0300

    shared preferences

--- app/src/main/java/cosmin/dev/nokia5gmobileapp/MainActivity.kt
+++ app/src/main/java/cosmin/dev/nokia5gmobileapp/MainActivity.kt
@@ -1,23 +1,34 @@
 package cosmin.dev.nokia5gmobileapp
 
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import androidx.preference.PreferenceManager
+import cosmin.dev.nokia5gmobileapp.data.SharedPreferencesManager
+import cosmin.dev.nokia5gmobileapp.navigation.Navigation
 import cosmin.dev.nokia5gmobileapp.ui.theme.Nokia5GMobileAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Nokia5GMobileAppTheme {
+                val navController = rememberNavController()
 
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    Navigation(navController = navController)
+                }
 
             }
         }
