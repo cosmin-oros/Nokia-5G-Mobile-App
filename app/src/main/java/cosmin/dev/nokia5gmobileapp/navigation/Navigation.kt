2e930eff7527bac30f3d133a4b1e0f6b79df5e5b
Cosmin <oroscosmin09@gmail.com>
2023-06-26 16:45:50 +0300

    splash screen

--- app/src/main/java/cosmin/dev/nokia5gmobileapp/navigation/Navigation.kt
+++ app/src/main/java/cosmin/dev/nokia5gmobileapp/navigation/Navigation.kt
@@ -7,13 +7,14 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
+import cosmin.dev.nokia5gmobileapp.ui.screens.other.SplashScreen
 
 @Composable
 fun Navigation(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screen.SplashScreen.route){
-//        composable(Screen.SplashScreen.route) {
-//            SplashScreen(navController = navController)
-//        }
+        composable(Screen.SplashScreen.route) {
+            SplashScreen(navController = navController)
+        }
 //
 //        composable(Screen.WelcomeScreen.route) {
 //            WelcomeScreen(navController = navController)
