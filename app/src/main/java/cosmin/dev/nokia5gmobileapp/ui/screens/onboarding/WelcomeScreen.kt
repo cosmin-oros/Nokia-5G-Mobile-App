7a7d7d8cbf7097fd484ab78078c2f0ac8bce7042
Cosmin <oroscosmin09@gmail.com>
2023-06-27 09:31:04 +0300

    welcome screen work

--- app/src/main/java/cosmin/dev/nokia5gmobileapp/ui/screens/onboarding/WelcomeScreen.kt
+++ app/src/main/java/cosmin/dev/nokia5gmobileapp/ui/screens/onboarding/WelcomeScreen.kt
@@ -1,9 +1,12 @@
 package cosmin.dev.nokia5gmobileapp.ui.screens.onboarding
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -25,24 +29,21 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import cosmin.dev.nokia5gmobileapp.R
+import cosmin.dev.nokia5gmobileapp.ui.theme.DarkGreen
+import cosmin.dev.nokia5gmobileapp.ui.theme.LightGreen
 
 @Composable
 fun WelcomeScreen(navController: NavController) {
-//    val sharedPrefs = SharedPreferencesManager.getSharedPrefs()
-//
-//    // Read the preference value
-//    val value = SharedPreferencesManager.getString("key", "default value")
-
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp)
-            .padding(bottom = 32.dp), // Add bottom padding to push the content up
-        verticalArrangement = Arrangement.SpaceBetween, // Use SpaceBetween to position elements at top and bottom
+            .padding(bottom = 32.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Column(
-            verticalArrangement = Arrangement.Center, // Center the top content vertically
+            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = Modifier.height(32.dp))
@@ -57,26 +58,45 @@ fun WelcomeScreen(navController: NavController) {
 
             Text(
                 text = "Nokia 5G/4G",
-                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.padding(bottom = 32.dp)
+            )
+
+            Text(
+                text = "Race against opponents using your internet speed and win rewards!",
+                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.SemiBold),
                 modifier = Modifier.padding(bottom = 32.dp)
             )
         }
 
         Button(
-            // navigate
-            onClick = {  },
+            onClick = { },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 16.dp)
                 .height(50.dp)
-                .shadow(4.dp, shape = MaterialTheme.shapes.medium)
-                .border(2.dp, Color.Cyan, shape = MaterialTheme.shapes.large),
-            colors = ButtonDefaults.buttonColors(Color.Black),
+                .shadow(4.dp, shape = MaterialTheme.shapes.medium),
+            colors = ButtonDefaults.buttonColors(Color.Transparent),
+            contentPadding = PaddingValues(0.dp)
         ) {
-            Text(
-                text = "Get Started",
-                style = MaterialTheme.typography.titleMedium.copy(color = Color.White, fontSize = 18.sp)
-            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                DarkGreen,
+                                LightGreen
+                            )
+                        )
+                    )
+            ) {
+                Text(
+                    text = "Get Started",
+                    style = MaterialTheme.typography.titleMedium.copy(color = Color.White, fontSize = 18.sp),
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
         }
     }
 }
