1befea3ec327e9819ead55e280ff22239d7078d2
Cosmin <oroscosmin09@gmail.com>
2023-06-27 17:30:48 +0300

    congratulations screen init

---
+++ app/src/main/java/cosmin/dev/nokia5gmobileapp/ui/screens/onboarding/CongratulationsScreen.kt
@@ -0,0 +1,112 @@
+package cosmin.dev.nokia5gmobileapp.ui.screens.onboarding
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import cosmin.dev.nokia5gmobileapp.R
+import cosmin.dev.nokia5gmobileapp.data.SharedPreferencesManager
+import cosmin.dev.nokia5gmobileapp.navigation.Screen
+import cosmin.dev.nokia5gmobileapp.ui.theme.DarkGreen
+import cosmin.dev.nokia5gmobileapp.ui.theme.LightGreen
+
+@Composable
+fun CongratulationsScreen(navController: NavController) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .padding(bottom = 32.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Logo",
+                modifier = Modifier.size(200.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Congratulations!",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Your car is ready!",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+        }
+
+
+        Button(
+            onClick = {
+                // set the user as logged in
+                SharedPreferencesManager.setString("logged_in", "yes")
+                navController.navigate(Screen.MainScreen.route)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp)
+                .height(50.dp)
+                .shadow(4.dp, shape = MaterialTheme.shapes.medium),
+            colors = ButtonDefaults.buttonColors(Color.Transparent),
+            contentPadding = PaddingValues(0.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                DarkGreen,
+                                LightGreen
+                            )
+                        )
+                    )
+            ) {
+                Text(
+                    text = "Start Playing",
+                    style = MaterialTheme.typography.titleMedium.copy(color = Color.White, fontSize = 18.sp),
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+    }
+}
