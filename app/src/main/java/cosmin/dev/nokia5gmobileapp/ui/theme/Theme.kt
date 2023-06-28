c9b3c4f6148bd2b255789a2d5f7f982ac990513e
Cosmin <oroscosmin09@gmail.com>
2023-06-28 11:42:14 +0300

    set theme to always dark

--- app/src/main/java/cosmin/dev/nokia5gmobileapp/ui/theme/Theme.kt
+++ app/src/main/java/cosmin/dev/nokia5gmobileapp/ui/theme/Theme.kt
@@ -47,11 +47,11 @@ fun Nokia5GMobileAppTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicDarkColorScheme(context)
         }
 
         darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> DarkColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
