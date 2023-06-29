ab4c8d5e3238e6a89bb8f042ebf065e564a13ab8
Cosmin <oroscosmin09@gmail.com>
2023-06-29 14:20:57 +0300

    actual download and upload speeds

--- app/src/main/java/cosmin/dev/nokia5gmobileapp/ui/screens/main/SinglePlayerScreen.kt
+++ app/src/main/java/cosmin/dev/nokia5gmobileapp/ui/screens/main/SinglePlayerScreen.kt
@@ -1,6 +1,8 @@
 package cosmin.dev.nokia5gmobileapp.ui.screens.main
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
@@ -11,17 +13,17 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import cosmin.dev.nokia5gmobileapp.utils.measureInternetSpeed
 import cosmin.dev.nokia5gmobileapp.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-@SuppressLint("ResourceType")
+@SuppressLint("UnrememberedMutableState", "CoroutineCreationDuringComposition")
 @Composable
 fun SinglePlayerScreen(navController: NavController) {
     var carWithInternetPosition by remember { mutableStateOf(0f) }
@@ -30,11 +32,20 @@ fun SinglePlayerScreen(navController: NavController) {
     val carImage = painterResource(R.drawable.ic_launcher_foreground) // Replace with your car image resource
     val canvasSize = 300.dp
 
+    val connectivityManager = LocalContext.current.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
     LaunchedEffect(Unit) {
-        GlobalScope.launch(Dispatchers.Default) {
-            measureInternetSpeed { internetSpeed ->
-                carWithInternetPosition = internetSpeed / 10f
-            }
+        while (true) {
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+
+            val downloadSpeed = networkCapabilities?.linkDownstreamBandwidthKbps
+            val uploadSpeed = networkCapabilities?.linkUpstreamBandwidthKbps
+
+            val internetSpeed = downloadSpeed?.toFloat() ?: 0f // Change to use both download and upload speeds
+
+            carWithInternetPosition = internetSpeed / 10f
+
+            delay(1000) // Delay to simulate the interval between measurements
         }
     }
 
@@ -60,4 +71,3 @@ fun SinglePlayerScreen(navController: NavController) {
         drawImage(carImageBitmap, Offset(carWithSetSpeedX, 200f))
     }
 }
-
