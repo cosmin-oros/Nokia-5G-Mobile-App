e2787a3eb81e03b8581c1c974d6d4d9569f1916b
Cosmin <oroscosmin09@gmail.com>
2023-07-03 13:08:57 +0300

    smoother animation

--- app/src/main/java/cosmin/dev/nokia5gmobileapp/ui/screens/main/SinglePlayerScreen.kt
+++ app/src/main/java/cosmin/dev/nokia5gmobileapp/ui/screens/main/SinglePlayerScreen.kt
@@ -15,6 +15,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,6 +32,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import cosmin.dev.nokia5gmobileapp.R
 import cosmin.dev.nokia5gmobileapp.data.SharedPreferencesManager
+import cosmin.dev.nokia5gmobileapp.navigation.Screen
 import cosmin.dev.nokia5gmobileapp.utils.CarAnimation
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -41,11 +45,11 @@ fun SinglePlayerScreen(navController: NavController) {
     var carWithInternetPosition by remember { mutableStateOf(0f) }
     var carWithSetSpeedPosition by remember { mutableStateOf(0f) }
 
-    val maxDistance = 300.dp // Maximum distance to the right
+    val maxDistance = 220f // Maximum distance to the right
 
     // Calculate the dynamic distance based on the carWithSetSpeedPosition
-    val dynamicDistance = (carWithSetSpeedPosition * 10).dp.coerceAtMost(maxDistance)
-    val dynamicDistanceYourCar = (carWithInternetPosition * 10).dp.coerceAtMost(maxDistance)
+    val dynamicDistance = (carWithSetSpeedPosition * 10).dp.coerceAtMost(maxDistance.dp)
+    val dynamicDistanceYourCar = (carWithInternetPosition * 10).dp.coerceAtMost(maxDistance.dp)
 
     val connectivityManager = LocalContext.current.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
@@ -60,9 +64,9 @@ fun SinglePlayerScreen(navController: NavController) {
             val internetSpeed = (uploadSpeed?.toFloat()?.let { downloadSpeed?.toFloat()?.plus(it) }) ?: 0f // upload speed + download speed
 
             // still way too fast and need to fix the fact that the car gets smaller
-            carWithInternetPosition += (internetSpeed / 100000f)
+            carWithInternetPosition += (internetSpeed / 10000000f)
 
-            delay(1000) // Delay to simulate the interval between measurements
+            delay(10) // Delay to simulate the interval between measurements
         }
     }
 
@@ -74,9 +78,9 @@ fun SinglePlayerScreen(navController: NavController) {
             val averageDownloadSpeed5g = 0f
             val averageUploadSpeed5g = 0f
 
-            // change 3f to averageDown + averageUp / 10000f
-            carWithSetSpeedPosition += 0.3f // Adjust the set speed as needed
-            delay(1000) // Delay to control the speed of the game loop
+            // change 0.3f to averageDown + averageUp / 10000000f !!!
+            carWithSetSpeedPosition += 0.003f // Adjust the set speed as needed
+            delay(10) // Delay to control the speed of the game loop
         }
     }
 
@@ -98,7 +102,7 @@ fun SinglePlayerScreen(navController: NavController) {
                 .padding(end = dynamicDistanceYourCar),
             horizontalArrangement = Arrangement.End
         ) {
-            CarAnimation(color = SharedPreferencesManager.getString("car_color", "black"), size = 200.dp)
+            CarAnimation(color = SharedPreferencesManager.getString("car_color", "black"), size = 100.dp)
         }
 
         Spacer(modifier = Modifier.height(32.dp))
@@ -109,8 +113,32 @@ fun SinglePlayerScreen(navController: NavController) {
                 .padding(end = dynamicDistance),
             horizontalArrangement = Arrangement.End
         ) {
-            CarAnimation(color = "black", size = 200.dp)
+            CarAnimation(color = "black", size = 100.dp)
+        }
+
+        if (carWithInternetPosition >= maxDistance || carWithSetSpeedPosition >= maxDistance) {
+            val dialogShown = remember { mutableStateOf(false) }
+            if (!dialogShown.value) {
+                AlertDialog(
+                    onDismissRequest = { dialogShown.value = false },
+                    title = { Text("Congratulations!") },
+                    text = { Text("You won!") },
+                    confirmButton = {
+                        Button(
+                            onClick = {
+                                dialogShown.value = false
+                                navController.navigate(Screen.MainScreen.route)
+                            }
+                        ) {
+                            Text("OK")
+                        }
+                    }
+                )
+                dialogShown.value = true
+            }
         }
     }
+
+
 
 }
